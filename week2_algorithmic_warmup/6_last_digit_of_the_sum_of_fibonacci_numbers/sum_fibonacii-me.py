7282966d298edc0e14d6ef80345820d1aff16575
anazeus1 <anasgh31@gmail.com>
2022-02-11 20:46:28 +0100

    using pasino period

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/sum_fibonacii-me.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/sum_fibonacii-me.py
@@ -1,15 +1,32 @@
 from time import time
 
-
-def fib(n):
+def huge(n,t):
     i=2
-    t=[0,1]
-    for i in range(2,n+1):
-        c=(t[i-1]+t[i-2])%10
-        t.append(c)
-    return t
-n=int(input())
-if n==0:
-    print(0)
+    no_period=True
+    while i<n+1 and no_period:
+        c=(t[i-1]+t[i-2])
+        t.append(c%10)
+        if c==1 and t[i-1]==0:
+            return i-1
+        else:
+            i=i+1
+    return -1
+n = int(input())
+t=[0,1]
+l=huge(n,t)
+if (t[len(t)-1])==1 and t[len(t)-2]==0:
+    t.pop(len(t)-1)
+    t.pop(len(t)-1)
+
+if n==1:
+    print(1)
+
+
+elif l==-1:
+    print(sum(t)%10)
 else:
-    print(sum(fib(n))%10)
+    a=0
+    s=sum(t)*(n//l)
+    for i in range (n%l+1):
+        a+=t[i]
+    print((s+a)%10)
